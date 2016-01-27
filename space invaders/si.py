e9dda6e4a80234b71ef637a94f8d6d77152c8e17
culshoefer <christoph.ulshoefer.15@ucl.ac.uk>
2016-01-27 23:34:47 +0000

    Added aliens, shooting, lives
    todo:
    Remove aliens that are hit
    Add barrier system
    Use score system for hitting aliens
    Add alien that comes very infrequently

--- space invaders/si.py
+++ space invaders/si.py
@@ -1,69 +1,70 @@
 import pygame
 import sys
-pygame.init()
+import random
 
+
+pygame.init()
 size = width, height = 448, 512
-speed = [2, 2]
 black = 0, 0, 0
 
 class Entity(pygame.sprite.Sprite):
-    def __init__(self, lives, x, y, img, altimg):
+    def __init__(self, lives, x, y, img):
         pygame.sprite.Sprite.__init__(self)
-        self.moveDelay = 0
         self.x = x
         self.y = y
-        self.dx= 1
+        self.dx = 4
+        self.dy = 0
         self.img = pygame.Surface([0, 0])
         self.img = pygame.image.load(img)
-        self.altimg = pygame.image.load(altimg)
         self.width = self.img.get_width()
         self.height= self.img.get_height()
         self.rect = self.img.get_rect()
         self.rect.x = self.x
         self.rect.y = self.y
-        self.posBoundaryLeft = self.width/2
-        self.posBoundaryRight= width - self.width/2
+        self.posBoundaryLeft = 20 + self.width/2
+        self.posBoundaryRight= width - (20 + self.width/2)
         self.width = self.img.get_width()
         self.height= self.img.get_height()
-        self.posBoundaryLeft = self.width/2
-        self.posBoundaryRight= width - self.width/2
         self.rect = self.img.get_rect()
         self.rect.x = self.x
         self.rect.y = self.y
-        self.entities = pygame.sprite.Group()
         self.lives = lives
         
     def update(self):
         self.move()
         self.checkBoundaries()
         self.rect.center = (self.x, self.y)
-        self.setInterval(0)
-        pygame.display.update()
                  
     def intersects(self, Entity):
         return self.rect.colliderect(Entity.rect)
     
     def move(self):
         self.x = self.x + self.dx
+        self.y = self.y + self.dy
 
     def checkBoundaries(self):
         if self.x < self.posBoundaryLeft:
             self.x = self.posBoundaryLeft
+            self.setIntervalX(4)
         if self.x > self.posBoundaryRight:
             self.x = self.posBoundaryRight
-        
-    def setMoveDelay(self, moveDelay):
-        self.moveDelay = moveDelay
+            self.setIntervalX(-4)
 
-    def setInterval(self, newInterval):
+    def setIntervalX(self, newInterval):
         self.dx = newInterval
+
+    def setIntervalY(self, newInterval):
+        self.dy = newInterval
         
     def setPosition(self, x, y):
         self.x = x
         self.y = y
 
+    def removeLife(self):
+        self.lives = self.lives - 1
+
 class Game():
-    def __init__(self, startScore, lifeImage, lives, player):
+    def __init__(self, startScore, lifeImage, player, aliens):
         self.lifeImage = pygame.image.load(lifeImage)
         self.font = pygame.font.SysFont("monospace", 20)
         self.text = self.font.render("SCORE", 1, (255, 255, 255))
@@ -72,73 +73,112 @@ class Game():
         self.textpos.y = 30
         self.score = startScore
         self.ticks = 0
-        self.lives = lives
         self.player= player
+        self.lastShot = self.ticks
+        self.aliens= aliens
+        self.playerShots = []
+        self.alienShots = []
         self.running = True
         self.screen = pygame.display.set_mode(size)
         
     def addScore(self, toAdd):
         self.score = self.score + toAdd
 
-    def removeLife(self):
-        self.lives = self.lives - 1
-        if self.lives == 0:
-            self.running = False
 
+    def makeRandomShots(self):
+        if(random.random() > 0.997):
+            row = random.randint(0, 4)
+            col = random.randint(0, 10)
+            self.alienShots.append(Entity(1, self.aliens[col][row].x, self.aliens[col][row].y, "shot.png"))
+            self.alienShots[-1].setIntervalX(0)
+            self.alienShots[-1].setIntervalY(0.75)
+
+    def computeInput(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: game.running = False
+        pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[pygame.K_a] or pressed_keys[pygame.K_LEFT]:
+            self.player.setIntervalX(-0.5)
+        if pressed_keys[pygame.K_d] or pressed_keys[pygame.K_RIGHT]:
+            self.player.setIntervalX(0.5)
+        if pressed_keys[pygame.K_SPACE] and self.ticks - 200 > self.lastShot:
+            self.playerShots.append(Entity(1, self.player.x, self.player.y, "shot.png"))
+            self.playerShots[-1].setIntervalX(0)
+            self.playerShots[-1].setIntervalY(-1)
+            self.lastShot = self.ticks
+            
     def update(self):
         self.screen.fill(black)
         self.screen.blit(self.text, self.textpos)
+        self.computeInput()
+        self.player.update()
         self.screen.blit(self.player.img, self.player.rect)
-
-        for i in range(0, self.lives -1):
+        self.player.setIntervalX(0)
+        self.makeRandomShots()
+        for row in self.aliens:
+            for col in row:
+                if self.ticks % 300 == 0:
+                    col.update()
+                self.screen.blit(col.img, col.rect)
+        for shot in self.alienShots:
+            if shot.y > height or shot.y < 0:
+                self.alienShots.remove(shot)
+                continue
+            shot.update()
+            self.screen.blit(shot.img, shot.rect)
+            if self.player.intersects(shot):
+                self.alienShots.remove(shot)
+                self.player.removeLife()
+        for shot in self.playerShots:
+            if shot.y > height or shot.y < 0:
+                self.playerShots.remove(shot)
+                continue
+            shot.update()
+            self.screen.blit(shot.img, shot.rect)
+            for row in self.aliens:
+                for col in row:
+                    if shot.intersects(col):
+                        #self.aliens.remove(col)
+                        self.playerShots.remove(shot)
+        if self.player.lives == 0:
+            self.running = False
+        self.ticks += 1
+        #self.sprites.update()
+        #self.sprites.draw()
+        for i in range(0, self.player.lives):
             livesRect = self.lifeImage.get_rect()
-            livesRect.x = self.textpos.x + 200 + i * 30
-            livesRect.y = self.textpos.y + 200 + i * 30
-            pygame.display.update()
+            livesRect.x = self.textpos.x + 150 + i * 30
+            livesRect.y = self.textpos.y
+            self.screen.blit(self.lifeImage, livesRect)
 			
     def isRunning(self):
         return self.running
 	
-	
-
-
+aliens = []
+for i in range(11):
+    new = []
+    for j in range(5):
+        newAlien = Entity(1, width/2 -1400 + 30 * i, height/2 - 130 + 30 * j, "sprite" + str(j+1) + ".png")
+        newAlien.posBoundaryLeft = 50 + 30 * i
+        newAlien.posBoundaryRight= width -50 - 30 * (10 - i)
+        new.append(newAlien)
+    aliens.append(new)
 player = Entity(3, width/2, 400, "player.png")
-game = Game(0, "heart.jpg", 3, player)
+game = Game(0, "heart.png", player, aliens)
+#game.sprites.add(player)
+#for row in aliens:
+#    for column in row:
+#        game.sprites.add(column)
 
-running = True
 while game.isRunning():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: game.running = False
-    pressed_keys = pygame.key.get_pressed()
-    if pressed_keys[pygame.K_a] or pressed_keys[pygame.K_LEFT]:
-        game.player.setInterval(-1)
-    if (pressed_keys[pygame.K_d] or pressed_keys[pygame.K_RIGHT]):
-        game.player.setInterval(1)
-    player.update()
     game.update()
     pygame.display.flip()
 
-
 pygame.quit()
-#11x5 enemies
-#fire stuff randomly
-#fire randomly every 30 seconds
-#enemies[]
-
-#class menu:
-#    start game
-#    high score
-#class highscore:
-#    rankInHS
-    
-#class Entity:
-#    width, height, position
-#    image
-#    points
-#    damage received
-#class mover:
-#    move around
-#    fire
+#todo:  Remove aliens that are hit by shots
+#       Add barriers
+#       Add score system
+#       Add animations
     
 
 
