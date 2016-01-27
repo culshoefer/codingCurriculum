0bd06ad25f5c8d0829ae2de3abe992af5156e3d2
Janos Potecki <janospotecki@Janoss-MacBook-Pro.local>
2016-01-27 13:42:19 +0000

    Init of the Snake class now working

--- snake/snake.py
+++ snake/snake.py
@@ -10,6 +10,17 @@ pygame.init()
 #######################################################
 
 
+# gamefield:
+# x|y
+# 0|0 1|0 2|0 3|0 4|0 5|0 6|0 7|0 .. width|0
+# 0|1 1|1 2|1 3|1 4|1 5|1 6|1 7|1       :
+# 0|2 1|2 2|2 3|2 4|2 5|2 6|2 7|2       :
+# 0|3 1|3 2|3 3|3 4|3 5|3 6|3 7|3       :
+#  :                              .     :
+#  :                                .   :
+# 0|height ..........................width|height
+
+
 # class position, makes it easier to understand the code 
 # of the class Snake
 class Position:
@@ -23,6 +34,10 @@ class Position:
     def changePosition(change_x, change_y):
         self.x = change_x
         self.y = change_y
+    def __repr__(self):
+        return str(self.X()) + "|" + str(self.Y()) + " "
+    def __str__(self):
+        return str(self.X()) + "|" + str(self.Y()) + " "
 
 class Direction:
     def __init__(self, x_direction, y_direction):
@@ -40,15 +55,43 @@ class Direction:
         tmp_x = self.x
         self.x = -1 * self.y
         self.y = tmp_x
+    def __eq__(self, direction):
+        return self.x == direction.X() and self.y == direction.Y() 
+
+class SnakeBody:
+    def __init__(self, headposition, num_of_bodyparts):
+        self.arr = []
+        for i in range(1, num_of_bodyparts+1):
+            self.arr.append(Position( headposition.X()-i, headposition.Y() ))
+    def getNumOfBodyparts(self):
+        return len(arr)
+    def addBodypart(self, position):
+        self.arr.append(position)
+    def getBodypart(self, number):
+        if number < len(arr) and number >= 0:
+            px = self.arr[number]
+            py = self.arr[number]
+            return Position(px, py)
+    def __repr__(self):
+        s = ""
+        for i in range(0, len(self.arr)):
+            s += str(self.arr[i])
+        return s
+    def __str__(self):
+        s = ""
+        for i in range(0, len(self.arr)):
+            s += str(self.arr[i])
+        return s
+
 
 # class snake
 # body : 2D arr holding position of body parts ~ to the head 
 # direction : arr holding the direction [x, y] 
-class snake:
-    def __init__(self, x_pos, y_pos):
-        self.direction = [1, 0]
+class Snake:
+    def __init__(self, x_pos, y_pos, body=4):
+        self.direction = Direction(1, 0)
         self.position = Position(x_pos, y_pos)
-
+        self.body = SnakeBody(self.position, body);
     def changeDirection(change_x, change_y):
         self.direction = [change_x, change_y]
     def moveForward(self):
@@ -57,15 +100,25 @@ class snake:
         self.direction.rotateClockwise
     def moveLeft(self):
         self.direction.rotateAnticlockwise
+    def __repr__(self):
+        return "Head: " + str(self.position) + " Body: " + str(self.body) 
+    def __str__(self):
+        return "Head: " + str(self.position) + " Body: " + str(self.body) 
 
 
 
 
 # game constants
+square_size = 10
 screen_width = 320
 screen_height= 240
-field_columns = screen_width / 10
-field_rows = screen_height / 10
+field_columns = screen_width / square_size
+field_rows = screen_height / square_size
+
+LEFT = Direction(-1, 0)
+RIGHT = Direction(1, 0)
+DOWN = Direction(0, 1)
+UP = Direction(0, -1)
 
 gamefield = [[0 for x in range(field_columns)] for x in range(field_rows)]
 
@@ -74,7 +127,7 @@ BLACK = (0x00, 0x00, 0x00)
 
 
 
-# SETUP
+
 
 #setup the screen
 size = (screen_width, screen_height)
@@ -103,6 +156,9 @@ while not done:
                 print("KEYRIGHT")
 
     #Game logic should go here
+    snake = Snake(int(field_columns/2), int(field_rows/2))
+    print snake
+
 
     # Drawing code should go here
     screen.fill(BLACK)
