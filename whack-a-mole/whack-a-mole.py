351757e85f060cd657d7aeb588a520e15ee46b20
jermenkoo <latal.jaromir@gmail.com>
2016-03-20 15:51:12 +0000

    better coding style

--- whack-a-mole/whack-a-mole.py
+++ whack-a-mole/whack-a-mole.py
@@ -1,8 +1,10 @@
 from __future__ import division
 
 import random
-
 import pygame
+
+NUMBER_OF_IMAGES = 4
+IMAGE_SIZE = 152
 
 
 class Mole(pygame.sprite.Sprite):
@@ -43,15 +45,15 @@ screen = pygame.display.set_mode((568 + 50, 568 + 50))
 all_sprites = pygame.sprite.Group()
 
 board = [[None for i in range(4)] for j in range(4)]
-for i in range(0, 608, 152):
-    for j in range(0, 608, 152):
+for i in range(0, NUMBER_OF_IMAGES * IMAGE_SIZE, IMAGE_SIZE):
+    for j in range(0, NUMBER_OF_IMAGES * IMAGE_SIZE, IMAGE_SIZE):
         temp = Mole(i + 10, j + 10)
 
         if random.random() < 0.5:
             temp.show(True)
             all_sprites.add(temp)
 
-        board[i // 152][j // 152] = temp
+        board[i // IMAGE_SIZE][j // IMAGE_SIZE] = temp
 
 last_position = (-1, -1)
 while True:
@@ -64,7 +66,7 @@ while True:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             # get the position of the click and corresponding tile
             last_position = x, y = pygame.mouse.get_pos()
-            current_tile = board[x // 152][y // 152]
+            current_tile = board[x // IMAGE_SIZE][y // IMAGE_SIZE]
 
             if current_tile.visible:
                 all_sprites.remove(current_tile)
@@ -75,7 +77,8 @@ while True:
             check_win()
 
             # map the (x, y) into array indices
-            x, y = map(lambda x: x // 152, last_position)
+            old_x, old_y = last_position
+            x, y = old_x // IMAGE_SIZE, old_y // IMAGE_SIZE
             # BFS around current tile
             for dx, dy in zip([0, 0, 1, -1], [-1, 1, 0, 0]):
                 # if the tile we try to modify is inside the board
